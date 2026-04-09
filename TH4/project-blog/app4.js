248b279e489a65c5e9389683cb2833cda8a72de9
Minow-2628 <trungkien11088@gmail.com>
2026-04-09 19:01:24 +0700

    update TH2 TH3 TH4

--- TH4/project-blog/app4.js
+++ TH4/project-blog/app4.js
@@ -6,66 +6,58 @@ const app = express();
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public')); // để dùng styles.css
-
-// View engine
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-// Kết nối MongoDB
+// MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/blogDB')
-.then(() => console.log('Kết nối MongoDB thành công'))
-.catch((error) => console.log(error));
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log(err));
 
 // ===== Routes =====
 
 // Trang danh sách bài viết
 app.get('/', async (req, res) => {
     const posts = await BlogPost.find({}).sort({ createdAt: -1 });
-    res.render('index', { posts });
+    const msg = req.query.msg;
+    res.render('index', { posts, msg });
 });
 
-// Trang tạo bài viết mới
-app.get('/blogposts/new', (req, res) => {
-    res.render('create');
-});
+// Tạo bài viết mới
+app.get('/blogposts/new', (req, res) => res.render('create'));
 
 // Lưu bài viết mới
 app.post('/blogposts/store', async (req, res) => {
-    await BlogPost.create({
-        title: req.body.title,
-        body: req.body.body
-    });
-    res.redirect('/');
+    await BlogPost.create({ title: req.body.title, body: req.body.body });
+    res.redirect('/?msg=Bài viết đã được tạo thành công');
 });
 
-// Trang chi tiết bài viết
+// Chi tiết bài viết
 app.get('/blogposts/:id', async (req, res) => {
     const post = await BlogPost.findById(req.params.id);
     res.render('detail', { post });
 });
 
-// Trang chỉnh sửa bài viết
+// Trang chỉnh sửa
 app.get('/blogposts/edit/:id', async (req, res) => {
     const post = await BlogPost.findById(req.params.id);
     res.render('edit', { post });
 });
 
-// Lưu chỉnh sửa bài viết
+// Lưu chỉnh sửa
 app.post('/blogposts/update/:id', async (req, res) => {
     await BlogPost.findByIdAndUpdate(req.params.id, {
         title: req.body.title,
         body: req.body.body
     });
-    res.redirect('/');
+    res.redirect('/?msg=Bài viết đã được cập nhật thành công');
 });
 
 // Xóa bài viết
 app.post('/blogposts/delete/:id', async (req, res) => {
     await BlogPost.findByIdAndDelete(req.params.id);
-    res.redirect('/');
+    res.redirect('/?msg=Bài viết đã được xóa thành công');
 });
 
 // Server
-app.listen(3000, () => {
-    console.log('Server đang chạy tại http://localhost:3000');
-});
+app.listen(3000, () => console.log('Server running at http://localhost:3000'));
